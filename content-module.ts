84d9abcc2144532824fa1e3626eed9a5a4286942
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-05-12 20:49:57 +0000

    0.4.0

--- content-module.ts
+++ content-module.ts
@@ -2,8 +2,24 @@ import { ContentModule } from "@content-app/types";
 
 const module: ContentModule = {
     name: 'ModuleTeaser',
-    async fetch({ fetchFromContentful }) {
-      return await fetchFromContentful();
+    async fetch({ fetchFromContentful, client, moduleData }: any) {
+      if (!moduleData.fields.teaser) {
+        return moduleData;
+      }
+
+      for(let i = 0; i < moduleData.fields.teaser.length; i++) {
+        const teaser = await client.getEntry(moduleData.fields.teaser[i].sys.id);
+        if (!teaser.fields.pageLink) {
+          continue;
+        }
+
+        const pageLink = teaser.fields.pageLink.sys.id;
+        const page = await client.getEntry(pageLink);
+        teaser.fields.pageLink = page;
+        moduleData.fields.teaser[i] = teaser;
+      }
+
+      return moduleData;
     }
   };
   
