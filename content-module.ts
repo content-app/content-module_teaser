eb8280b18970a568b7216cd1106b2f99c51f96c9
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-05-19 11:50:13 +0000

    0.4.1

--- content-module.ts
+++ content-module.ts
@@ -9,13 +9,13 @@ const module: ContentModule = {
 
       for(let i = 0; i < moduleData.fields.teaser.length; i++) {
         const teaser = await client.getEntry(moduleData.fields.teaser[i].sys.id);
-        if (!teaser.fields.pageLink) {
-          continue;
+        
+        if (teaser.fields.pageLink) {
+          const pageLink = teaser.fields.pageLink.sys.id;
+          const page = await client.getEntry(pageLink);
+          teaser.fields.pageLink = page;
         }
-
-        const pageLink = teaser.fields.pageLink.sys.id;
-        const page = await client.getEntry(pageLink);
-        teaser.fields.pageLink = page;
+        
         moduleData.fields.teaser[i] = teaser;
       }
 
